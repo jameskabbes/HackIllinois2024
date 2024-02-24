40b3e2cbf562498fb1549be59a15fddfae5e790a
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2024-02-24 01:59:13 -0600

    led

--- code/motor_spin.py
+++ code/motor_spin.py
@@ -1,6 +1,7 @@
 import numpy as np
 from src import motor as motor_module
 from src import motor_rotations
+from src import led as led_module
 import time
 
 if __name__ == '__main__':
@@ -21,6 +22,14 @@ if __name__ == '__main__':
         }
     })
 
+    led1 = led_module.LED({
+        "pin": 20
+    })
+
+    led2 = led_module.LED({
+        "pin": 21
+    })
+
     speeds = list(np.linspace(0, 1, 11)) + list(np.linspace(0.9, 0, 10))
 
     dt = 0.25
@@ -39,12 +48,15 @@ if __name__ == '__main__':
     #     motor1.backward(speed)
     #     motor2.backward(speed)
     #     time.sleep(dt)
-
+    led1.on()
     motor_rotations.rotate_ccw_90_deg(motor1, motor2)
     time.sleep(dt)
     motor_rotations.rotate_cw_90_deg(motor1, motor2)
     time.sleep(dt)
+    led1.off()
+    led2.on()
     motor_rotations.move_forward(motor1, motor2, 5)
     time.sleep(dt)
     motor_rotations.move_backward(motor1, motor2, 5)
     time.sleep(dt)
+    led2.off()
