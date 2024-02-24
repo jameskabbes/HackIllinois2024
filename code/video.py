51206901c257ec4f604f957c9a89e86462e501b3
Pranav <114105021+pranavuiuc@users.noreply.github.com>
2024-02-23 23:50:04 -0600

    Update video.py

--- code/video.py
+++ code/video.py
@@ -1,19 +1,24 @@
-from ultralytics import YOLO 
+from ultralytics import YOLO
 from enum import Enum
+import cv2
 
 class ModelType(Enum): 
     YOLOv8n = 'yolov8n.pt'     
     YOLOv8s = 'yolov8s.pt' 
     YOLOv8x = 'yolov8x.pt' 
 
-class Camera(Enum): 
-    LOGI_1 = '0'
-    LAPTOP = '1' 
-    LOGI_2 = '2' 
-
-def liveObjDetection(modelType: ModelType):
+def detectPersonInFrame(frame, modelType: ModelType):
     model = YOLO(modelType.value)
-    model.predict(source=Camera.LOGI_1.value, show=True)
+    results = model(frame)
+    person_detected = any(cls == 0 for cls in results[0].boxes.cls)
+    if person_detected:
+        return 1
+    else:
+        return 0
 
-if __name__ == '__main__': 
-    liveObjDetection(ModelType.YOLOv8n)
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cap.release()
+    cv2.destroyAllWindows()
+    print(detectPersonInFrame(frame, ModelType.YOLOv8n))
