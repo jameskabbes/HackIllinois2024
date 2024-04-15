63ed920555c19e162d0a2affacce657172d51358
James Kabbes <james.kabbes@gmail.com>
2024-04-15 13:10:40 -0500

    making updates

--- code/test_encoder.py
+++ code/test_encoder.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as gpio
+import time
 
-ENCODER_PIN = 17
+ENCODER_PIN = 6
 tick_count = 0
 
 
@@ -10,9 +11,8 @@ def encoder_callback(channel):
 
 
 def setup_gpio():
-    gpio.set_mode(gpio.BCM)
+    gpio.setmode(gpio.BCM)
     gpio.setup(ENCODER_PIN, gpio.IN, pull_up_down=gpio.PUD_UP)
-    gpio.add_event_detect(ENCODER_PIN, gpio.FALLING, callback=encoder_callback)
 
 
 if __name__ == '__main__':
@@ -20,6 +20,10 @@ if __name__ == '__main__':
 
     try:
         while True:
+
+            print(gpio.input(ENCODER_PIN))
+            time.sleep(1)
+
             print(f'Tick count: {tick_count}')
 
     except KeyboardInterrupt:
