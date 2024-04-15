ea9df1473fc10bbfba6589effd8217507fce04ea
James Kabbes <james.kabbes@gmail.com>
2024-04-15 15:18:56 -0500

    adding encoder functionality

--- code/src/wheel_encoder.py
+++ code/src/wheel_encoder.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from gpiozero import Button
+import math
 
 
 class WheelEncoder(Button):
@@ -8,6 +9,7 @@ class WheelEncoder(Button):
     pin: int
 
     TICKS_PER_REVOLUTION: int = 192
+    WHEEL_RADIUS: float = 25.0  # mm
 
     def __init__(self, pin: int):
 
@@ -16,9 +18,12 @@ class WheelEncoder(Button):
         self.ticks = 0
 
     def increment_ticks(self):
-        print('tick detected!')
         self.ticks += 1
 
-    @property
-    def revolutions(self):
-        return self.ticks / WheelEncoder.TICKS_PER_REVOLUTION
+    @staticmethod
+    def revolutions_from_ticks(ticks):
+        return ticks / WheelEncoder.TICKS_PER_REVOLUTION
+
+    @staticmethod
+    def distance_from_ticks(ticks):
+        return 2*math.pi*WheelEncoder.WHEEL_RADIUS * WheelEncoder.revolutions_from_ticks(ticks)
