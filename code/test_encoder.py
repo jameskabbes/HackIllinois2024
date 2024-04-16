8aa51694eb05d3a78e6d587698e0c34c1b893009
James Kabbes <james.kabbes@gmail.com>
2024-04-16 09:30:57 -0500

    modiyfing encoder test

--- code/test_encoder.py
+++ code/test_encoder.py
@@ -5,12 +5,14 @@ import RPi.GPIO as GPIO
 
 if __name__ == '__main__':
 
-    total_seconds = 60
+    total_seconds = 10
     start_time = time.time()
 
-    left_encoder = wheel_encoder.WheelEncoder(14)
-    right_encoder = wheel_encoder.WheelEncoder(4)
+    left_encoder = wheel_encoder.WheelEncoder({'pin': 14})
+    right_encoder = wheel_encoder.WheelEncoder({'pin': 4})
 
-    print(left_encoder.ticks, right_encoder.ticks)
-    time.sleep(10)
-    print(left_encoder.ticks, right_encoder.ticks)
+    while time.time() - start_time < total_seconds:
+        print('Left Encoder Ticks: ', left_encoder.ticks)
+        print('Right Encoder Ticks: ', right_encoder.ticks)
+
+        time.sleep(.1)
